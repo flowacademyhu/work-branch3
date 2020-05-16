4437eee2339549bea718d6eeac46cb64c5f7bab2
levee98 <leventet@t-email.hu>
2020-05-16 14:25:09 +0200

    mul and sub

--- team1/index.js
+++ team1/index.js
@@ -3,11 +3,19 @@
 const sum = (a,b)=>{
     return a+b;
 
+// mul
+const mul = (a, b) => {
+    return a * b;
+};
 
+// sub
+const sub = (a, b) => {
+    return a - b;
+};
 
 module.exports = {
     sum: sum,
     mul: mul,
     div: div,
-    sub: sum
+    sub: sub
 };
