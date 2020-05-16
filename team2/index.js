f13fd3b2d656df75c57a436254095f58b2d60e11
Junászka Klaudia <klausz8801@gmail.com>
2020-05-16 14:41:00 +0200

    fixes

--- team2/index.js
+++ team2/index.js
@@ -1,3 +1,17 @@
+const summary = (a, b) => {
+  return a + b;
+};
+
 const mul = (a, b) => {
   return a * b;
 };
+
+const sub = (a, b) => { // sub new  for team
+  return a - b;
+};
+
+module.exports = {
+  summary,
+  mul: mul,
+  sub: sub
+}
