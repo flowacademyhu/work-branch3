7a5e9db6818792944c39ccc42d359972ade0a2ce
Kriván Zoltán <krivan.zoltan@gmail.com>
2020-05-16 14:39:15 +0200

    fix

--- teamwork2/index.js
+++ teamwork2/index.js
@@ -2,9 +2,15 @@ const summary = (a, b) => {
 	return a+b;
 
 module.export = {
-	summary
+    summary,
+    div,
+    sub
 }
 
 const sub = (a, b) => { // sub new  for team
   return a - b;
 };
+
+const div = (a,b) => {  // div for team projekt branch3 madeByKZ
+    return a/b;
+    };
