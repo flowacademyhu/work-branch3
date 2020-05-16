348ee3781438c4244f1f79093c01190361be0b80
Dani <farkika39@gmail.com>
2020-05-16 14:37:56 +0200

    adding missing };

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const sum = (a, b) => { // writen by Daniel
   return a + b;
 };
+
 const sub = (a, b) => { // writen by Daniel
   return a - b;
 };
+
 const mul = (a, b) => {
   return a * b;
 };
 
 const div = (a, b) => {
   return a / b;
+};
 
 module.exports = {
   sum,
